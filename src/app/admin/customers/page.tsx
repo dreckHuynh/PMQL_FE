aa6694c77a445cb63f1070529801332208e0fd78
Duchnh <hduy01012000@gmail.com>
2025-03-03 06:34:40 +0700

    Update UI

--- src/app/admin/customers/page.tsx
+++ src/app/admin/customers/page.tsx
@@ -542,9 +542,7 @@ export default function CustomerManagement() {
           >
             {customers.map((item, index) => (
               <TableRow key={index}>
-                <TableCell>
-                  {total - ((Number(page) - 1) * rowsPerPage + index + 1)}
-                </TableCell>
+                <TableCell>total - (limit * (page - 1)) - index</TableCell>
                 <TableCell>
                   {item.full_name || "-"}
                   {item.full_name && (
